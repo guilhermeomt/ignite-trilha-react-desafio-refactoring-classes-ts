75e7c416df12925c89a901f21885755bfdd59eae
Guilherme <guilhermet213@gmail.com>
2021-08-07 20:05:04 -0300

    refactor: convert ModalEditFood component to FC and tsx

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,62 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import { Modal } from '../Modal';
+import { Input } from '../Input';
+
+interface Food {
+  name: string;
+  price: string;
+  description: string;
+  image: string;
+}
+
+interface ModalEditFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleUpdateFood: (food: Food) => void;
+  editingFood: Food;
+}
+
+export function ModalEditFood({ isOpen, setIsOpen, handleUpdateFood, editingFood }: ModalEditFoodProps) {
+  const formRef = useRef(null);
+
+  const handleSubmit = async (food: Food) => {
+    handleUpdateFood(food);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input
+          name="image"
+          placeholder="Cole o link aqui"
+        />
+
+        <Input
+          name="name"
+          placeholder="Ex: Moda Italiana"
+        />
+        <Input
+          name="price"
+          placeholder="Ex: 19.90"
+        />
+
+        <Input
+          name="description"
+          placeholder="Descrição"
+        />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
